faaf1d911442a61c81f53defc3d72372337a6827
Yena Chang <yena.chang@universite-paris-saclay.fr>
2022-04-18 11:51:43 +0200

    added small test

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from PIL import Image, ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 from scipy import signal
 import seaborn as sns
+from color_sort import *
+from clusters import*
+from envelopes import *
 
-images = load_images(media, "*.png")
-image_grid(images)
+img = Image.open("media/a01.png")
+
+print(points(img))
