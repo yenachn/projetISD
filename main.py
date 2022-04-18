67685ace149d53e00d05960fb2dd59166214b080
Extends <55919390+ImExtends@users.noreply.github.com>
2022-04-18 13:32:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 # yena chang MI1
 # vincent villiaumey LDDIM1
 # projet 2 isd
-import os
+import os, re
+from glob import glob as ls
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+from sys import path
 from PIL import Image, ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 from scipy import signal
-import seaborn as sns
+import seaborn as sns; sns.set()
 from color_sort import *
 from clusters import*
 from envelopes import *
 
 # load tous les images
+
+dataset_dir = 'media'
+images = load_images(dataset_dir, "*.png")
 
 # points(img) pour tous les images (c'est par boucle? paslu les tps)
 
